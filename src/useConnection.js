eb3455aeb8cf2a4b47071177fb201b5b05184987
Shushant-Priyadarshi <profshushant@gmail.com>
2024-07-21 19:04:39 +0530

    please work :(

--- src/useConnection.js
+++ src/useConnection.js
@@ -1,19 +1,17 @@
 import { useState, useEffect } from "react";
 
 const useConnection = () => {
-  const [status, setStatus] = useState(window.navigator.onLine);
+  const [status, setStatus] = useState(true);
 
   useEffect(() => {
-    const handleOnline = () => setStatus(true);
-    const handleOffline = () => setStatus(false);
+    window.addEventListener("online",()=>{
+      setStatus(true)
+    })
 
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
-
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
+    window.addEventListener("offline",()=>{
+      setStatus(false)
+    })
+   
   }, []);
 
   return status;
